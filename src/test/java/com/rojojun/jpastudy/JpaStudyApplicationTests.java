6c682a2bfef9b42c57dba8a46ab066bb0e8ed8dd
rojojun <zoloman316@gmail.com>
2024-03-23 01:25:55 +0900

    first commit

---
+++ src/test/java/com/rojojun/jpastudy/JpaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rojojun.jpastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
